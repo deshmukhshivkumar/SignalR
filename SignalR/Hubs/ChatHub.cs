102f6f272da4094723a24a9997ca76a0891e72cb
Shiv <sdeshmukh@tavisca.com>
2014-12-23 09:57:24 +0530

    first commit

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string userName , string message)
+        {
+            Clients.All.showMessage(userName, message + " <i>sent at " + DateTime.Now +"</i>");
+            
+         
+        }
+    }
+}
---
+++ SignalR/Hubs/SimpleHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class SimpleHub : Hub
+    {
+        public void Hello()
+        {
+            // Client will invoke server method Hello through javascript   and   server will respond by invoking client method called introduce and passing it string
+            Clients.All.introduce("Hello World !!!");
+            //Clients.Caller.introduce("Hello World !!!");
+            //Clients.Others.introduce("Hello World !!!");
+        }
+    }
+}
